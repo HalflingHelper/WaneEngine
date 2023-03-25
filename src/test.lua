3aecf5b719d4dfa7d3bd0e954e34f7c905e3e41c
Calvin Josenhans <60663079+HalflingHelper@users.noreply.github.com>
2023-03-25 13:04:42 -0400

    Pseudocode for perft method

--- src/test.lua
+++ src/test.lua
@@ -1,5 +1,23 @@
 -- Contains test functions (perft) and a series of instructions for running tests
 
-function perft()
+--[[
+    A Perft function for testing purposes
+    Walks the move generation tree to depth depth, and counts the number of strictly legal moves
 
+    Implementation : https://www.chessprogramming.org/Perft
+    Expected output: https://www.chessprogramming.org/Perft_Results
+]]
+
+function perft(depth)
+    if depth == 0 then return 0 end
+
+    local nodes = 0
+
+    --moves = genLegalMoves()
+    -- for every move in moves
+        -- make the move
+        -- nodes += perft(depth - 1)
+        -- undo the move
+
+    return nodes
 end
