1ac45267634a6203a533f69c2ba4afebb2bd507f
Calvin Josenhans <60663079+HalflingHelper@users.noreply.github.com>
2023-04-23 21:56:09 -0400

    hashing setup

---
+++ src/hash.lua
@@ -0,0 +1,15 @@
+local hashPiece = {}
+local hashCastle = {nil, nil, nil, nil}
+local hashSide
+local hashEp
+
+--
+function init_hash()
+
+end
+
+-- Additive zobrist hash
+-- https://www.chessprogramming.org/Zobrist_Hashing 
+function get_hash(board)
+
+end
